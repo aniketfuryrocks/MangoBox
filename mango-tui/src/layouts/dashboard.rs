0f7e9626a106b023ad2520bbdf9c7a319f02fbc7
Aniket Prajapati <contact@aniketprajapati.me>
2022-09-21 08:24:23 +0530

    app state

--- mango-tui/src/layouts/dashboard.rs
+++ mango-tui/src/layouts/dashboard.rs
@@ -1,10 +1,17 @@
+use tui::layout::{Constraint, Direction, Layout};
 use tui::widgets::{Block, Borders};
 use tui::Frame;
 
-use crate::app::AppBackend;
+use crate::app::{AppBackend, AppState};
 
-pub fn draw(frame: &mut Frame<AppBackend>) {
-    let size = frame.size();
-    let block = Block::default().title("Mango").borders(Borders::ALL);
-    frame.render_widget(block, size);
+pub fn draw(frame: &mut Frame<AppBackend>, _state: &AppState) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints([Constraint::Percentage(10), Constraint::Percentage(80)].as_ref())
+        .split(frame.size());
+    let block = Block::default().title("Block").borders(Borders::ALL);
+    frame.render_widget(block, chunks[0]);
+    let block = Block::default().title("Block 2").borders(Borders::ALL);
+    frame.render_widget(block, chunks[1]);
 }
