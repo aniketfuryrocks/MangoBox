318c12d4c2149e8fe9688937c2af64f00acb946c
Lioncat2002 <sankhayan2002@gmail.com>
2022-09-21 23:11:15 +0530

    feat: fetching user data from mango api

--- mango-api/src/lib.rs
+++ mango-api/src/lib.rs
@@ -10,6 +10,19 @@ pub async fn fetch_price() -> Value {
     p
 }
 
+
+//https://mango-transaction-log.herokuapp.com/v3/user-data/profile-details
+// 5SKRvHAuiARiMYJtkXYuWy1kVTEJWbEAhnVN3MHxKzan
+pub async fn fetch_userdata(wallet_pk:String)->Value{
+    let url="https://mango-transaction-log.herokuapp.com/v3/user-data/profile-details".to_string();
+    let params = [("wallet-pk", wallet_pk)];
+    let url=reqwest::Url::parse_with_params(&url, params.iter()).unwrap();
+    let client = Client::new();
+    let response = client.get(url).send().await.unwrap().text().await.unwrap();
+    let p: Value = serde_json::from_str(response.as_str()).unwrap();
+    p
+}
+
 pub async fn fetch_leaderboard() -> u16 {
     let url = "https://mango-transaction-log.herokuapp.com/v3/stats/pnl-leaderboard".to_string();
     let c = Client::new();
@@ -26,9 +39,18 @@ mod tests {
         let status = fetch_leaderboard().await;
         assert_eq!(status, 200);
     }
+
     #[tokio::test]
     async fn test_prices() {
         let x = fetch_price().await;
+        println!("{:#?}", x);
+        //assert_eq!(status, 200);
+    }
+
+    #[tokio::test]
+    async fn test_userdata() {
+        let wallet_pk="5SKRvHAuiARiMYJtkXYuWy1kVTEJWbEAhnVN3MHxKzan".to_string();
+        let x = fetch_userdata(wallet_pk).await;
         println!("{:#?}", x);
         //assert_eq!(status, 200);
     }
