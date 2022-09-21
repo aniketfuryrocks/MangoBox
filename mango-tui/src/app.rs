9b7429da84a7d704ec41c0d75eb0eb116cfbdffb
Aniket Prajapati <contact@aniketprajapati.me>
2022-09-21 08:10:34 +0530

    distributed ui code into multiple modules

--- mango-tui/src/app.rs
+++ mango-tui/src/app.rs
@@ -1,16 +1,22 @@
 use crossterm::{
-    event::{DisableMouseCapture, EnableMouseCapture},
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::io::{self, Stdout};
-use tui::{
-    backend::CrosstermBackend,
-    widgets::{Block, Borders},
-    Terminal,
+use std::{
+    io::{self, Stdout},
+    time::{Duration, Instant},
 };
+use tui::{backend::CrosstermBackend, Terminal};
 
-pub struct App(Terminal<CrosstermBackend<Stdout>>);
+use crate::layouts;
+
+pub type AppBackend = CrosstermBackend<Stdout>;
+
+pub struct App {
+    terminal: Terminal<AppBackend>,
+    should_quit: bool,
+}
 
 impl App {
     pub fn new() -> anyhow::Result<Self> {
@@ -18,15 +24,17 @@ impl App {
         let mut stdout = io::stdout();
         execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
         let backend = CrosstermBackend::new(stdout);
-        Ok(Self(Terminal::new(backend)?))
+        let terminal = Terminal::new(backend)?;
+
+        Ok(Self {
+            terminal,
+            should_quit: false,
+        })
     }
 
+    #[inline]
     pub fn draw(&mut self) -> anyhow::Result<()> {
-        self.0.draw(|f| {
-            let size = f.size();
-            let block = Block::default().title("Mango").borders(Borders::ALL);
-            f.render_widget(block, size);
-        })?;
+        self.terminal.draw(layouts::dashboard::draw)?;
 
         Ok(())
     }
@@ -34,11 +42,49 @@ impl App {
     pub fn restore(&mut self) -> anyhow::Result<()> {
         disable_raw_mode()?;
         execute!(
-            self.0.backend_mut(),
+            self.terminal.backend_mut(),
             LeaveAlternateScreen,
             DisableMouseCapture
         )?;
-        self.0.show_cursor()?;
+        self.terminal.show_cursor()?;
         Ok(())
+    }
+
+    pub fn run(&mut self, tick_rate: Duration) -> anyhow::Result<()> {
+        self.should_quit = false;
+
+        let mut last_tick = Instant::now();
+
+        while !self.should_quit {
+            self.draw()?;
+
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if crossterm::event::poll(timeout)? {
+                if let Event::Key(key) = event::read()? {
+                    if let KeyCode::Char(c) = key.code {
+                        self.on_key(c)
+                    }
+                }
+            }
+            if last_tick.elapsed() >= tick_rate {
+                last_tick = Instant::now();
+            }
+        }
+
+        self.restore()
+    }
+
+    pub fn on_key(&mut self, key: char) {
+        if let 'q' = key {
+            self.quit()
+        }
+    }
+
+    #[inline]
+    pub fn quit(&mut self) {
+        self.should_quit = true;
     }
 }
