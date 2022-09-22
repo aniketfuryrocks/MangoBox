9288194e7f5fc13c17797221e7eccaa100e9b24f
Restyled.io <commits@restyled.io>
2022-09-22 15:31:11 +0000

    Restyled by clang-format

--- extension/popup.js
+++ extension/popup.js
@@ -1,22 +1,18 @@
 var button = document.getElementById("btn1");
 var text = document.getElementById("demo");
-button.addEventListener("click", function () {
-    let inputVal = document.getElementById("input").value;
-    if (inputVal === "omkar") {
-        text.style.display = "none";
-        chrome.tabs.update({ url: "https://www.youtube.com" });
-    } else {
-        toggleText();
-    }
-
+button.addEventListener("click", function() {
+  let inputVal = document.getElementById("input").value;
+  if (inputVal === "omkar") {
+    text.style.display = "none";
+    chrome.tabs.update({url : "https://www.youtube.com"});
+  } else {
+    toggleText();
+  }
 });
-
 
 function toggleText() {
 
-    if (text.style.display === "none") {
-        text.style.display = "block";
-    }
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  }
 }
-
-
