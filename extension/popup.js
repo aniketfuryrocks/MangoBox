01d683e9079ad77238e8319e45c5fd3440911869
Omkar Acharekar <omkaracharekar12@gmail.com>
2022-09-22 20:23:26 +0530

    wallet verfication css

--- extension/popup.js
+++ extension/popup.js
@@ -1,7 +1,22 @@
 var button = document.getElementById("btn1");
+var text = document.getElementById("demo");
 button.addEventListener("click", function () {
-    chrome.tabs.update({ url: "https://www.sollet.io/" });
+    let inputVal = document.getElementById("input").value;
+    if (inputVal === "omkar") {
+        text.style.display = "none";
+        chrome.tabs.update({ url: "https://www.youtube.com" });
+    } else {
+        toggleText();
+    }
 
 });
+
+
+function toggleText() {
+
+    if (text.style.display === "none") {
+        text.style.display = "block";
+    }
+}
 
 
