e9b2302741b2e87d17c2b7a5d74f972d92e88486
Omkar Acharekar <omkaracharekar12@gmail.com>
2022-09-22 17:24:50 +0530

    added wallet

--- extension/popup.js
+++ extension/popup.js
@@ -1,2 +1,7 @@
-const btn = document.querySelector('.changeColorBtn');
+var button = document.getElementById("btn1");
+button.addEventListener("click", function () {
+    chrome.tabs.update({ url: "https://www.sollet.io/" });
+
+});
+
 
