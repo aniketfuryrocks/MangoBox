b8f5babc085f7290ad239d416a40d754dbb6db76
Lioncat2002 <sankhayan2002@gmail.com>
2022-09-23 01:30:20 +0530

    feat: got a working bar chart

--- mango-api/src/lib.rs
+++ mango-api/src/lib.rs
@@ -1,4 +1,4 @@
-use reqwest::Client;
+use reqwest::blocking::Client;
 use serde::Deserialize;
 use serde_json::{json, Value};
 
@@ -12,10 +12,10 @@ pub struct User{
 }
 
 
-pub async fn fetch_price() -> Value {
+pub fn fetch_price() -> Value {
     let url = "https://all-market-stats-api.onrender.com/markets/".to_string();
     let client = Client::new();
-    let response = client.get(url).send().await.unwrap().text().await.unwrap();
+    let response = client.get(url).send().unwrap().text().unwrap();
 
     let p: Value = serde_json::from_str(response.as_str()).unwrap();
     p
@@ -23,53 +23,56 @@ pub async fn fetch_price() -> Value {
 
 //https://mango-transaction-log.herokuapp.com/v3/user-data/profile-details
 // 5SKRvHAuiARiMYJtkXYuWy1kVTEJWbEAhnVN3MHxKzan
-pub async fn fetch_userdata(wallet_pk: String) -> User {
-    let url =
-        "https://mango-transaction-log.herokuapp.com/v3/user-data/profile-details".to_string();
-    let params = [("wallet-pk", wallet_pk)];
-    let url = reqwest::Url::parse_with_params(&url, params.iter()).unwrap();
-    let client = Client::new();
-    let response = client
-        .get(url)
-        .send()
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap_or_else(|_| json!({"wallet_pk":"null"}).to_string()); //handling the theoretical possibility of a non existent wallet
-    let p: User = serde_json::from_str(response.as_str()).unwrap();
-    p
-}
+//pub async fn fetch_userdata(wallet_pk: String) -> User {
+//    let url =
+//        "https://mango-transaction-log.herokuapp.com/v3/user-data/profile-details".to_string();
+//    let params = [("wallet-pk", wallet_pk)];
+//    let url = reqwest::Url::parse_with_params(&url, params.iter()).unwrap();
+//    let client = Client::new();
+//    let response = client
+//        .get(url)
+//        .send()
+//        .await
+//        .unwrap()
+//        .text()
+//        .await
+//        .unwrap_or_else(|_| json!({"wallet_pk":"null"}).to_string()); //handling the theoretical possibility of a non existent wallet
+//    let p: User = serde_json::from_str(response.as_str()).unwrap();
+//    p
+//}
 
-pub async fn fetch_leaderboard() -> u16 {
-    let url = "https://mango-transaction-log.herokuapp.com/v3/stats/pnl-leaderboard".to_string();
-    let c = Client::new();
-    let res = c.get(url).send().await.unwrap();
-    println!("{:?}", res);
-    res.status().as_u16()
-}
+//pub async fn fetch_leaderboard() -> u16 {
+//    let url = "https://mango-transaction-log.herokuapp.com/v3/stats/pnl-leaderboard".to_string();
+//    let c = Client::new();
+//    let res = c.get(url).send().await.unwrap();
+//    println!("{:?}", res);
+//    res.status().as_u16()
+//}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[tokio::test]
-    async fn test_stats() {
-        let status = fetch_leaderboard().await;
-        assert_eq!(status, 200);
-    }
+ //   #[tokio::test]
+ //   async fn test_stats() {
+ //       let status = fetch_leaderboard().await;
+ //       assert_eq!(status, 200);
+ //   }
 
-    #[tokio::test]
-    async fn test_prices() {
-        let x = fetch_price().await;
-        println!("{:#?}", x);
+    #[test]
+    fn test_prices() {
+        let x = fetch_price();
+        for i in x.as_array().unwrap(){
+            println!("{:#?}", i["baseSymbol"]);
+        }
+        
         //assert_eq!(status, 200);
     }
 
-    #[tokio::test]
-    async fn test_userdata() {
-        let wallet_pk = "5SKRvHAuiARiMYJtkXYuWy1kVTEJWbEAhnVN3MHxKzan".to_string();
-        let x = fetch_userdata(wallet_pk).await;
-        println!("{:#?}", x);
-        //assert_eq!(status, 200);
-    }
+ //   #[tokio::test]
+ //   async fn test_userdata() {
+ //       let wallet_pk = "5SKRvHAuiARiMYJtkXYuWy1kVTEJWbEAhnVN3MHxKzan".to_string();
+ //       let x = fetch_userdata(wallet_pk).await;
+ //       println!("{:#?}", x);
+ //       //assert_eq!(status, 200);
+ //   }
 }
--- mango-tui/src/layouts/dashboard.rs
+++ mango-tui/src/layouts/dashboard.rs
@@ -1,12 +1,20 @@
 use tui::{
     layout::{Constraint, Direction, Layout},
-    widgets::{Block, Borders},
-    Frame,
+    widgets::{Block, Borders, BarChart},
+    Frame, style::{Style, Color, Modifier},
 };
+use mango_api::fetch_price;
 
 use crate::app::{AppBackend, AppState};
 
 pub fn draw(frame: &mut Frame<AppBackend>, _state: &AppState) {
+    let prices=fetch_price();
+    let mut chatdata:[(&str,u64);30]=[("",0);30];
+    let mut counter=0;
+    for i in prices.as_array().unwrap(){
+        chatdata[counter]=(i["baseSymbol"].as_str().unwrap_or(""),i["midPrice"].as_f64().unwrap() as u64);
+        counter+=1;
+    }
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .margin(1)
@@ -15,5 +23,18 @@ pub fn draw(frame: &mut Frame<AppBackend>, _state: &AppState) {
     let block = Block::default().title("Block").borders(Borders::ALL);
     frame.render_widget(block, chunks[0]);
     let block = Block::default().title("Block 2").borders(Borders::ALL);
-    frame.render_widget(block, chunks[1]);
+
+    let barchart=BarChart::default()
+    .block(Block::default().title("BarChart").borders(Borders::ALL))
+    .bar_width(10)
+    .bar_gap(1)
+    .bar_style(Style::default().fg(Color::Green))
+    .value_style(
+        Style::default()
+            .fg(Color::White)
+            .add_modifier(Modifier::BOLD),
+    )
+    .label_style(Style::default().fg(Color::White))
+    .data(&chatdata);
+    frame.render_widget(barchart, chunks[1]);
 }
