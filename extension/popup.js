bcb353e648f141ede3a692a4ee5fecd1a951f797
Restyled.io <commits@restyled.io>
2022-09-22 15:31:28 +0000

    Restyled by prettier

--- extension/popup.js
+++ extension/popup.js
@@ -1,17 +1,16 @@
 var button = document.getElementById("btn1");
 var text = document.getElementById("demo");
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   let inputVal = document.getElementById("input").value;
   if (inputVal === "omkar") {
     text.style.display = "none";
-    chrome.tabs.update({url : "https://www.youtube.com"});
+    chrome.tabs.update({ url: "https://www.youtube.com" });
   } else {
     toggleText();
   }
 });
 
 function toggleText() {
-
   if (text.style.display === "none") {
     text.style.display = "block";
   }
