26bcf872a1ab4e19d0476b737a02db20b127f6c7
Aniket Prajapati <contact@aniketprajapati.me>
2022-09-23 20:35:13 +0530

    fmt

--- extension/popup.js
+++ extension/popup.js
@@ -1,7 +1,8 @@
 let store = 0;
 var button = document.getElementById("btn1");
 var text = document.getElementById("demo");
-chrome.storage.sync.get(['key'], function (result) {
+
+chrome.storage.sync.get(['key'], function(result) {
     if (result.key === "omkar") {
         store = 1;
 
@@ -10,12 +11,12 @@ chrome.storage.sync.get(['key'], function (result) {
 });
 
 if (store == 0) {
-    button.addEventListener("click", function () {
+    button.addEventListener("click", function() {
         let inputVal = document.getElementById("input").value;
         if (inputVal === "omkar") {
             text.style.display = "none";
 
-            chrome.storage.sync.set({ key: inputVal }, function () {
+            chrome.storage.sync.set({ key: inputVal }, function() {
                 console.log('Value is set to ' + value);
             });
 
@@ -25,8 +26,8 @@ if (store == 0) {
         }
 
     });
-
 }
+
 function toggleText() {
 
     if (text.style.display === "none") {
