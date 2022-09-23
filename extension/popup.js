a02af06436e6658375b5dd4d82ff84266608b8d0
Omkar Acharekar <omkaracharekar12@gmail.com>
2022-09-23 17:20:26 +0530

    store user wallet public key in storage

--- extension/popup.js
+++ extension/popup.js
@@ -1,22 +1,39 @@
+let store = 0;
 var button = document.getElementById("btn1");
 var text = document.getElementById("demo");
-button.addEventListener("click", function () {
-    let inputVal = document.getElementById("input").value;
-    if (inputVal === "omkar") {
-        text.style.display = "none";
+chrome.storage.sync.get(['key'], function (result) {
+    if (result.key === "omkar") {
+        store = 1;
+        window.close();
         chrome.tabs.update({ url: "https://www.youtube.com" });
-    } else {
-        toggleText();
     }
-
 });
 
+if (store == 0) {
+    button.addEventListener("click", function () {
+        let inputVal = document.getElementById("input").value;
+        if (inputVal === "omkar") {
+            text.style.display = "none";
 
+            chrome.storage.sync.set({ key: inputVal }, function () {
+                console.log('Value is set to ' + value);
+            });
+
+            chrome.tabs.update({ url: "https://www.youtube.com" });
+            window.close();
+        } else {
+            toggleText();
+        }
+
+    });
+
+}
 function toggleText() {
 
     if (text.style.display === "none") {
         text.style.display = "block";
     }
 }
+
 
 
